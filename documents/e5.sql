8858ae005f9a765eb07a5b473583ee1f8944e0ef
plusdojo2025-miyazawa <miyazawa-yuuto-plusdojo2025@seplus2016.onmicrosoft.com>
2025-06-13 14:42:02 +0900

    データベース修正

--- documents/e5.sql
+++ documents/e5.sql
@@ -14,7 +14,19 @@
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
--- エクスポートするデータが選択されていません
+
+-- e5 のデータベース構造をダンプしています
+CREATE DATABASE IF NOT EXISTS `e5` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `e5`;
+
+--  テーブル e5.users の構造をダンプしています
+CREATE TABLE IF NOT EXISTS `users` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `username` varchar(30) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- テーブル e5.users: ~0 rows (約) のデータをダンプしています
 
 /*!40103 SET TIME_ZONE=IFNULL(@OLD_TIME_ZONE, 'system') */;
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
