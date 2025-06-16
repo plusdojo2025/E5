a934cd9f389604288e458cba33fd8bfdc51f92fe
plusdojo2025-wakabayashi <wakabayashi-ayuno-plusdojo2025@seplus2016.onmicrosoft.com>
2025-06-16 11:01:39 +0900

    model追加

--- source/src/main/java/model/UsernamePassword.java
+++ source/src/main/java/model/UsernamePassword.java
@@ -1,5 +1,34 @@
 package model;
 
-public class UsernamePassword {
+import java.io.Serializable;
 
+public class UsernamePassword implements Serializable{
+	private String username; // ID
+	private String pw; // パスワード
+
+	public String getId() {
+		return username;
+	}
+
+	public void setId(String username) {
+		this.username = username;
+	}
+
+	public String getPw() {
+		return pw;
+	}
+
+	public void setPw(String pw) {
+		this.pw = pw;
+	}
+
+	public  UsernamePassword(String username, String pw) {
+		this.username = username;
+		this.pw = pw;
+	}
+
+	public  UsernamePassword() {
+		this.username = "";
+		this.pw = "";
+	}
 }
