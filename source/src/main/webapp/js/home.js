d9c1dcfb3e55121a9f05efc82e57848b1279c35b
plusdojo2025-kitahara <kitahara-daiki-plusdojo2025@seplus2016.onmicrosoft.com>
2025-06-17 10:15:07 +0900

    Home

--- source/src/main/webapp/js/home.js
+++ source/src/main/webapp/js/home.js
@@ -1,3 +1,10 @@
-/**
- * 
- */
+'use strict'
+document.addEventListener('DOMContentLoaded', function () {
+    const itemsMenu = document.querySelector('.items_menu');
+    const items = document.querySelector('.items');
+
+    // メニューが開かれているか、いないかによって（条件式）displayの表示をnone（非表示）にしたりflex（表示）にしたりする
+    itemsMenu.addEventListener('click', function () {
+    items.style.display = items.style.display === 'flex' ? 'none' : 'flex';
+    });
+});
